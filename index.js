6c59354b52015624d1658b4c6503c8c02b1b04d7
Trevor Johnson <johnsontrevor55@gmail.com>
2018-10-11 19:38:11 -0700

    section 4

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 /*
- * Primary file for API
- *
- */
+  -- Modules being used --
+  Command Line Options
+  Console
+  Crypto
+  File System
+  Globals
+  HTTP
+  HTTPS
+  Modules
+  Path
+  Process
+  Query String
+  String Decoder
+  Timers
+  URL
+  Utilities
+  Buffer
+  Zlib
+  Stream
+*/
 
 // dependencies
 var server = require('./lib/server');
 var workers = require('./lib/workers');
+require('./lib/setEnv')();
 
 // declare the app;
 var app = {};
