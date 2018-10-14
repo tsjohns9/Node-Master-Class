3bde7a5751ba15970c6cc2214e2289ca4b32c6fa
Trevor Johnson <johnsontrevor55@gmail.com>
2018-10-13 22:33:32 -0700

    updates

--- index.js
+++ index.js
@@ -23,20 +23,29 @@
 // dependencies
 var server = require('./lib/server');
 var workers = require('./lib/workers');
+var cli = require('./lib/cli');
 require('./lib/setEnv')();
 
 // declare the app;
 var app = {};
 
 // init function
-app.init = function() {
+app.init = function(callback) {
   // start the server
   server.init();
   // start the workers
   workers.init();
+
+  // start the cli
+  setTimeout(function() {
+    cli.init();
+    callback();
+  }, 50);
 };
 
-// execute
-app.init();
+// self invoking only if required directly
+if (require.main === module) {
+  app.init(function() {});
+}
 
 module.exports = app;
